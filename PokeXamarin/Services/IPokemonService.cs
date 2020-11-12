f53d9bde9170d6d6bd239dcb8e900478c01bf840
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2020-11-12 10:59:38 -0300

    :sparkles: Implementado PokeApi

---
+++ PokeXamarin/Services/IPokemonService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using PokeXamarin.Model;
+
+namespace PokeXamarin.Services
+{
+    public interface IPokemonService
+    {
+        Task<List<Pokemon>> GetPokemonsAsync();
+    }
+}
--- PokeXamarin/ViewModels/MainViewModel.cs
+++ PokeXamarin/ViewModels/MainViewModel.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using PokeXamarin.Helpers;
+using PokeXamarin.Model;
+using PokeXamarin.Services;
 using PokeXamarin.ViewModels.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace PokeXamarin.ViewModels
 {
     public class MainViewModel : ViewModelBase, IMainViewModel
     {
-        public MainViewModel()
+        public ObservableCollection<Pokemon> Pokemons { get; }
+        IPokemonService _PokemonService;
+
+        public MainViewModel(ILogger<MainViewModel> logger)
         {
+            Title = "Poke Xamarin";
+            Pokemons = new ObservableCollection<Pokemon>();
+            _PokemonService = App.ServiceProvider.GetService<IPokemonService>();
+            logger.LogCritical("Acessando o Aplicativo");
+
         }
     }
 }
